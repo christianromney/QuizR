83c20942b01be9d009aabf7779edcb36f457f013
Christian Romney <cromney@pointslope.com>
2018-08-18 18:32:21 -0400

    Try using pygame for interruptible music
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- robotPerfect.py
+++ robotPerfect.py
@@ -1,5 +1,6 @@
 #from EmulatorGUI import GPIO
 import RPi.GPIO as GPIO
+import pygame
 import time
 import os
 from datetime import datetime
@@ -14,13 +15,14 @@ correct  = None
 question = None
 curdir   = os.path.dirname(os.path.realpath(__file__))
 
-random.seed(datetime.now())
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(QUESTION, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(ANSWER_A, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(ANSWER_B, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(ANSWER_C, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+def initialize():
+   GPIO.setmode(GPIO.BCM)
+   GPIO.setup(QUESTION, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+   GPIO.setup(ANSWER_A, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+   GPIO.setup(ANSWER_B, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+   GPIO.setup(ANSWER_C, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+   random.seed(datetime.now())
+   pygame.mixer.init()
 
 def find_random_question():
    pattern = os.path.sep.join([curdir, "questions", "**", "*.mp3"])
@@ -33,8 +35,10 @@ def find_random_question():
       return [correct, question]
 
 def play_sound(file):
+   pygame.mixer.music.stop()
    time.sleep(0.2)
-   os.system ('mpg123 ' + os.path.realpath(file))
+   pygame.mixer.music.load(os.path.realpath(file))
+   pygame.mixer.music.play()
 
 def was_pressed(button):
    return not GPIO.input(button)
@@ -46,29 +50,20 @@ def play_message(msg):
 def check_answer(correct, given):
    play_message("correct" if correct == given else "try-again")
 
-while True:
-   if was_pressed(QUESTION):
-      correct, question = find_random_question()
-      play_sound(question)
+if __file__ == "__main__":
+   initialize()
+   while True:
+      if was_pressed(QUESTION):
+         correct, question = find_random_question()
+         play_sound(question)
 
-   if was_pressed(ANSWER_A):
-      check_answer(correct, "a")
+      if was_pressed(ANSWER_A):
+         check_answer(correct, "a")
 
-   if was_pressed(ANSWER_B):
-      check_answer(correct, "b")
+      if was_pressed(ANSWER_B):
+         check_answer(correct, "b")
 
-   if was_pressed(ANSWER_C):
-      check_answer(correct, "c")
+      if was_pressed(ANSWER_C):
+         check_answer(correct, "c")
 
-
-
-       # if was_pressed(ANSWER_A):
-    #     previous_robot = button_pressed('robot', previous_robot)
-
-    # if was_pressed(ANSWER_B):
-    #     previous_human = button_pressed('human', previous_robot)
-
-    # if was_pressed(ANSWER_C):
-    #     previous_robot = button_pressed('robot', previous_robot)
-
-GPIO.cleanup()
+   #GPIO.cleanup()
