208093baf049a844d2a9cc022538829ff694e5b1
Christian Romney <cromney@pointslope.com>
2018-09-23 10:07:41 -0400

    Remove IP hackery
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- boot.sh
+++ boot.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -e
-clear
-ip=$(hostname -I)
-printf "\033[34mIP: \033[36m$ip\n"
-tput sgr0
 quizr="/home/pi/src/quizr/app.py"
 if [ -f "$quizr" ]; then
   logger -t QuizR -p user.info "[>>> Launching QuizR <<<]"
